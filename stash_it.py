e8b7bb7bfc8ed9c5b7dcab7b66ae510666021ecf
susa-nah <susannahbourke@hotmail.com>
2024-09-29 09:57:56 +1000

    Add bag copying process.

--- stash_it.py
+++ stash_it.py
@@ -1,7 +1,7 @@
 from src.helper_functions import *
 import bagit
 import time
-import uuid
+import subprocess
 
 logger = logging.getLogger(__name__)
 
@@ -35,6 +35,7 @@ def main():
     if len(ok_files) == 0:
         logger.info("No trigger files staged in transfer directory.")
     else:
+        logger.info(f"Transfers to process: {len(ok_files)}")
         for file in ok_files:
             tf = TriggerFile(os.path.join(TRANSFER_DIR, file))
             folder = tf.get_directory()
@@ -42,20 +43,30 @@ def main():
                 valid_transfers.append(folder)
                 valid_metadata.update({folder: tf.get_metadata()})
 
+    mc = MetadataChecker()
+
     for folder in valid_transfers:
         # generate and add a random uuid as External-Identifier
-        transfer_id = uuid.uuid4()
         metadata = valid_metadata.get(folder)
-        metadata.update({"External-Identifier": transfer_id})
-        # check it's not already a bag:
         try:
-            bag = bagit.Bag(folder)
-            logger.info(f"Processing existing bag at: {folder}")
-        except Exception as e:
-            bag = bagit.make_bag(folder, bag_info=metadata)
-            logger.info(f"Making new bag at: {folder}")
-        if bag.is_valid():
-            print("We have a valid bag!")
+            metadata = mc.validate(metadata)
+            try:
+                bag = bagit.Bag(folder)
+                logger.info(f"Processing existing bag at: {folder}")
+                for key in metadata.keys():
+                    bag.info[key] = metadata.get(key)
+                bag.save()
+            except Exception as e:
+                logger.info(f"Making new bag at: {folder}")
+                bag = bagit.make_bag(folder, bag_info=metadata)
+            if bag.is_valid():
+                subprocess.run(["rsync", "-vrlt", folder, ARCHIVE_DIR])
+            output_bag = os.path.join(ARCHIVE_DIR, folder)
+            output_bag = bagit.Bag(output_bag)
+            output_bag.validate()
+        except ValueError as e:
+            logger.error(f"Error validating metadata: {e}")
+        # check it's not already a bag:
 
 
 main()
