0b2d516f16b3bf81ba749235d504477f58c6c292
susa-nah <susannahbourke@hotmail.com>
2024-11-09 16:01:20 +1100

    Add outline for tests.

---
+++ tests/test_stash_it.py
@@ -0,0 +1,16 @@
+from stash_it import *
+import pytest
+
+# test_configure_db
+
+# test_insert_transfer
+
+# test_validate_dirs
+
+# test_timed_rsync_copy
+
+# test_compute_manifest_hash
+
+# test_cleanup+transfer
+
+# test_main
