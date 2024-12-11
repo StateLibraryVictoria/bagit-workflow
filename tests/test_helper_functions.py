c84aec4412cc9adaf374c531950b021d0adebb62
Susannah Bourke <sbourke@slv.vic.gov.au>
2024-12-11 14:51:58 +1100

    Unit tests for transfer handling, metadata

--- tests/test_helper_functions.py
+++ tests/test_helper_functions.py
@@ -10,29 +10,20 @@ import pytest
 @pytest.fixture
 def valid_metadata_no_uuid():
     data = {
-        "Source-Organization": "Home",
-        "Contact-Name": "Susannah Bourke",
+        "Contact-Name": "sbourke",
         "External-Description": "A test metadata package for a valid transfer.",
-        "Internal-Sender-Identifier": "/path/to/folder",
+        "External-Identifier": "RA-9999-12",
     }
     yield data
 
 
 @pytest.fixture
 def valid_trigger_file(tmp_path):
-    file = tmp_path / "valid_trigger.ok"
-    data = {
-        "Source-Organization": "Home",
-        "Contact-Name": "Susannah Bourke",
-        "External-Description": "A test metadata package for a valid transfer.",
-        "Internal-Sender-Identifier": "/path/to/folder",
-    }
-    output = json.dumps(data)
-    dir = tmp_path / "valid_trigger"
+    file = tmp_path / "RA.9999.99_valid_trigger.ok"
+    dir = tmp_path / "RA.9999.99_valid_trigger"
     dir.mkdir()
     file2 = dir / "data.txt"
     file2.write_text("some text")
-    file.write_text(output)
     yield file
 
 
@@ -57,10 +48,9 @@ def existing_bag_info_file(tmp_path):
     bagit.make_bag(
         dir,
         {
-            "Source-Organization": "Home",
-            "Contact-Name": "Susannah Bourke",
+            "Contact-Name": "sbourke",
             "External-Description": "A test metadata package for a valid transfer.",
-            "Internal-Sender-Identifier": "/path/to/folder",
+            "External-Identifier": "RA-9999-12",
         },
     )
     yield file
@@ -95,32 +85,27 @@ def id_parser():
 
 """TriggerFile tests"""
 
-def test_load_metadata(valid_trigger_file):
-    tf = TriggerFile(valid_trigger_file)
-    assert tf.get_metadata().get("Source-Organization") == "Home"
+def test_load_metadata_path_correct(valid_trigger_file, id_parser):
+    tf = TriggerFile(valid_trigger_file, id_parser)
+    assert tf.get_metadata().get("External-Description") == "RA.9999.99_valid_trigger"
 
 
-def test_header_validation_fails(invalid_trigger_file):
-    tf = TriggerFile(invalid_trigger_file)
+def test_header_validation_fails(invalid_trigger_file, id_parser):
+    tf = TriggerFile(invalid_trigger_file, id_parser)
     tf.load_metadata()
     assert tf.validate() == False
 
 
-def test_header_validation_succeeds(valid_trigger_file):
-    tf = TriggerFile(valid_trigger_file)
+def test_validation_succeeds(valid_trigger_file, id_parser):
+    tf = TriggerFile(valid_trigger_file, id_parser)
     tf.load_metadata()
     assert tf.validate() == True
 
 
-def test_get_metadata_from_ok(valid_trigger_file, valid_metadata_no_uuid):
-    data = valid_metadata_no_uuid
-    tf = TriggerFile(valid_trigger_file)
-    assert data == tf.get_metadata()
 
-
-def test_get_metadata_from_bag(existing_bag_info_file, valid_metadata_no_uuid):
+def test_get_metadata_from_bag(existing_bag_info_file, valid_metadata_no_uuid, id_parser):
     data = valid_metadata_no_uuid
-    tf = TriggerFile(existing_bag_info_file)
+    tf = TriggerFile(existing_bag_info_file, id_parser)
     # clean up the extra keys we don't need
     result = tf.get_metadata()
     for key in ["Payload-Oxum", "Bagging-Date", "Bag-Software-Agent"]:
@@ -128,58 +113,29 @@ def test_get_metadata_from_bag(existing_bag_info_file, valid_metadata_no_uuid):
     assert data == result
 
 
-def test_error_file_creation(invalid_trigger_file, tmp_path):
-    tf = TriggerFile(invalid_trigger_file)
+def test_error_file_creation(invalid_trigger_file, tmp_path, id_parser):
+    tf = TriggerFile(invalid_trigger_file, id_parser)
     tf.validate()
     file = tmp_path / "invalid_trigger.error"
     assert os.path.isfile(file)
 
 
-def test_error_file_data(invalid_trigger_file, tmp_path):
-    tf = TriggerFile(invalid_trigger_file)
+def test_error_file_data(invalid_trigger_file, tmp_path, id_parser):
+    tf = TriggerFile(invalid_trigger_file, id_parser)
     tf.validate()
     file = tmp_path / "invalid_trigger.error"
-    expected = f'Headers do not match config.{os.linesep}Error parsing metadata. Are your values filled in?{os.linesep}{{"Source-Organization": "Home", "Contact-Name": "Input this value.", "External-Description": "Input this value.", "Internal-Sender-Identifier": "Input this value."}}'
+    expected = f'Error parsing metadata.{os.linesep}'
     with open(file, "r") as f:
         data = f.read()
     assert expected == data
 
 
-def test_not_ok_file_raises_exception(tmp_path):
+def test_not_ok_file_raises_exception(tmp_path, id_parser):
     file = tmp_path / "error_raise.txt"
     with pytest.raises(ValueError):
-        tf = TriggerFile(file)
-
-"""MetadataChecker tests"""
-
-def test_add_uuid_if_not_present(valid_metadata_no_uuid):
-    data = valid_metadata_no_uuid
-    mc = MetadataChecker()
-    metadata_with_uuid = mc.validate(data)
-    result = metadata_with_uuid.get("External-Identifier")
-    assert result is not None
-
-
-def test_add_uuid_has_other_ids(valid_metadata_no_uuid):
-    data = valid_metadata_no_uuid
-    data.update({"External-Identifier": "Some other id"})
-    mc = MetadataChecker()
-    metadata_with_uuid = mc.validate(data)
-    result = metadata_with_uuid.get("External-Identifier")
-    assert len(result) == 2
-
-
-def test_dont_add_uuid_if_exists(valid_metadata_no_uuid):
-    data = valid_metadata_no_uuid
-    data.update({"External-Identifier": str(uuid.uuid4())})
-    mc = MetadataChecker()
-    metadata_with_uuid = mc.validate(data)
-    result = metadata_with_uuid.get("External-Identifier")
-    result_uuid = uuid.UUID(result)
-    assert type(result_uuid) == uuid.UUID
+        tf = TriggerFile(file, id_parser)
 
 """IdParser Tests"""
-
 
 @pytest.mark.parametrize(
     "input, expected",
